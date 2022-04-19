ce5904865a53e601a3e03ccd06ff802c22ce9cab
Thuan Tran <tranthuan12002@gmail.com>
2022-04-19 14:04:36 +0300

    ex1.5

--- part1/reactjs/src/App.js
+++ part1/reactjs/src/App.js
@@ -27,33 +27,35 @@ const Total = (props) => {
 const Part = (props) => {
   return (
     <>
-      {props.parts} {props.exercises} <br />
+      {props.part} {props.exercises} <br />
     </>
   );
 };
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
