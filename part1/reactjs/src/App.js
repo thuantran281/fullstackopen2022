ff9dd678728d8bdf7261dce1990a412601a6c53d
Thuan Tran <tranthuan12002@gmail.com>
2022-06-07 10:22:41 +0300

    commit

--- part1/reactjs/src/App.js
+++ part1/reactjs/src/App.js
@@ -1,49 +1,44 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Header = (props) => {
-  console.log(props)
-  return <h1>{props.feedback}</h1>
-}
-
-const Header1 = (props) => {
-  console.log(props)
-  return <h1>{props.statistics}</h1>
-}
-
-const Text = (props) => {
-  return (
-    <>
-      {props.text}
-    </>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = () => {
-  // save clicks of each button to its own state
-  const feedback = "give feedback"
-  const statistics = "statistics"
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+  ];
 
+  const [selected, setSelected] = useState(0);
+  const [vote, setVote] = useState([0, 0, 0, 0, 0, 0, 0]);
+  //let vote = Array.apply(null, Array(anecdotes.length)).map(Number.prototype.valueOf,0);
+  //this outputs [0, 0, 0, 0, 0, 0, 0]
+
+  const getRandomAnecdote = () => {
+    const randomAnecdote = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomAnecdote);
+  }
+
+  const Vote = () => {
+    const voteCopy = [...vote];
+    voteCopy[selected] += 1;
+    setVote(voteCopy);
+  }
+  
   return (
     <div>
-      <Header feedback={feedback} />
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
-      <Header1 statistics={statistics} />
-      <Text text="good"/> {good}<br /> 
-      <Text text="neutral"/> {neutral} <br /> 
-      <Text text="bad"/> {bad}
+      {anecdotes[selected]} <br />
+      <p>has {vote[selected]} votes</p>
+      <Button handleClick={Vote} text="vote"/>
+      <Button handleClick={getRandomAnecdote} text="next anecdote"/>
     </div>
   )
-}
+};
 
-export default App
+export default App;
