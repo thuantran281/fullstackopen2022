cd5e0580082051b2d0200179195bb8a1c6f5b391
Thuan Tran <tranthuan12002@gmail.com>
2022-06-28 01:14:15 +0300

    ex 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+    const personObject = {
+      name: newName,
+      id: persons.length + 1,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <label>
+          <div>
+            name:{" "}
+            <input type="text" value={newName} onChange={handleNameChange} />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </label>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
